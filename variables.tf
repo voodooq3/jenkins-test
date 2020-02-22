efa7dd999408a36875708083ddb3c1e04ddd03be
voodooq3 <30564988+voodooq3@users.noreply.github.com>
2020-02-22 21:17:09 +0200

    commit

--- variables.tf
+++ variables.tf
@@ -1,18 +1,15 @@
 
-variable region {
-  description = "Region"
-  # type = string
-  default = "eu-central-1"
-}
+# variable region {
+#   description = "Region"
+#   default = "eu-central-1"
+# }
 
-variable shared_credentials_file_path {
-  description = "Shared Credentials File Path"
-  # type = "string"
-  default = "./key/cred.txt"
-}
+# variable shared_credentials_file_path {
+#   description = "Shared Credentials File Path"
+#   default = "./key/cred.txt"
+# }
 
-variable private_key_path {
-  description = "Pivate Key"
-  # type = "string"
-  default = "./key/voodookey.pem"
-}
+# variable private_key_path {
+#   description = "Pivate Key"
+#   default = "./key/voodookey.pem"
+# }
