0bd63b5d8714e3631f0da35266cd1559fb1da569
voodooq3 <30564988+voodooq3@users.noreply.github.com>
2020-02-23 17:18:00 +0200

    changes

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-
-# output "instance_ips" {
-#   description = "The public IP address of the main server instance."
-#   value = ["${aws_instance.standalone.*.public_ip}"]
-# }
-
-
-
--- standalone.tf
+++ standalone.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region                  = "eu-central-1"
+  version = "~> 2.0"
+}
+
 # #--- data ---#
 # data "aws_ami" "centos" {
 #   most_recent = true
@@ -29,40 +34,43 @@
 #     Project = "standalone"
 #   }
 
-  
+#--- security_group ---#
+resource "aws_security_group" "StandaloneGroup" {
+  name = "lab01 security group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Standalone Group"
+  }
+}
 
-  
+
+
+# output "instance_ips" {
+#   description = "The public IP address of the main server instance."
+#   value = ["${aws_instance.standalone.*.public_ip}"]
 # }
 
 
-# #--- security_group ---#
-# resource "aws_security_group" "StandaloneGroup" {
-#   name = "lab01 security group"
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#     ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#     ingress {
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "Standalone Group"
-#   }
-# }
